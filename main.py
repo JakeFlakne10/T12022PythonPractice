f10b70a9a034d273843f0560d212db3798636c7d
JakeFlakne10 <819483@mystma.org>
2022-09-13 09:40:59 -0500

    Made 5 functions + ran them

---
+++ main.py
@@ -0,0 +1,32 @@
+
+def oneTwo():
+    uno = 1
+    dos = 2
+    print (uno+dos)
+
+def threeFour():
+    tres = 3
+    cuatro = 4
+    print (tres+cuatro)
+
+def fiveSix():
+    cinco = 5
+    ceis = 6
+    print (cinco+ceis)
+
+def farmAnimals():
+    cow = "moo"
+    chicken = "bock"
+    print (cow+chicken)
+
+def housePets():
+    dog = "woof"
+    cat = "meow"
+    print (dog+cat)
+
+
+oneTwo()
+threeFour()
+fiveSix()
+farmAnimals()
+housePets()
